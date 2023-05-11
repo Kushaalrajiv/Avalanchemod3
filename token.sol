58a075dbb0e98e3d4dd91b5b01b367dbc18548b2
Kushaalrajiv <113694596+Kushaalrajiv@users.noreply.github.com>
2023-05-11 09:21:57 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.8.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.8.3/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Mindboggler is ERC20 {
-    constructor() ERC20("Mindboggler", "M14") {
+contract Mindboggler is ERC20, ERC20Burnable {
+    constructor() ERC20("Mindboggler", "m14") {
         _mint(msg.sender, 500 * 10 ** decimals());
     }
 }
